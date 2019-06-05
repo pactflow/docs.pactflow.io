ae6f58e32234ce249c21286fe6f7a12169351f4c
Ronald Holshausen <uglyog@gmail.com>
2019-06-05 17:23:21 +1000

    fix: always pull the image

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 set -e
-set -x
-
-whoami
-
-echo ------------------------------------------------------
-ls -l
-echo ------------------------------------------------------
-ls -l /usr/local/bin
-echo ------------------------------------------------------
 
 ScriptDir=$(dirname $0)
 
-cd $ScriptDir/../website && /usr/local/bin/yarn install && /usr/local/bin/yarn run build
+cd $ScriptDir/../website && yarn install && yarn run build
