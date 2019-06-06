d05338b3e6bbf3799687428ffe77bc6d30879a3d
Ronald Holshausen <uglyog@gmail.com>
2019-06-06 17:02:45 +1000

    fix: missing a slash

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,7 +4,7 @@ set -e
 
 ScriptDir=$(dirname $0)
 
-aws s3 sync s3://$dev_BUCKET s3://$prod_BUCKET
+aws s3 sync s3://$dev_BUCKET s3://$prod_BUCKET \
   --acl public-read \
   --cache-control "max-age=60, s-max-age=60, must-revalidate" \
   --delete
