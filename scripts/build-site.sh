c1b87404381f3e5aa3fd6fb0827c7bd6689e238c
Ronald Holshausen <uglyog@gmail.com>
2019-06-06 13:02:12 +1000

    fix: missing s3 URI prefix

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -12,7 +12,7 @@ yarn install && yarn run build
 BUCKET_VAR=${ENVIRONMENT}_BUCKET
 BUCKET=${!BUCKET_VAR}
 
-aws s3 sync ./build/doc-site $BUCKET \
+aws s3 sync ./build/doc-site s3://$BUCKET \
   --acl public-read \
   --cache-control "max-age=60, s-max-age=60, must-revalidate" \
   --delete
