33501ec4399a0635211ba8116f68bf340bbe2ca0
Ronald Holshausen <uglyog@gmail.com>
2019-06-06 16:15:44 +1000

    chore: add the sync to prod step

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-set -x
 
 ScriptDir=$(dirname $0)
 
---
+++ scripts/sync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+ScriptDir=$(dirname $0)
+
+rm -rf website/build/prod
+mkdir website/build/prod
+
+aws s3 sync s3://$dev_BUCKET website/build/prod
+
+find website/build/prod
