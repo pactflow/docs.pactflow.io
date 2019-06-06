52839382acf0674b374a1cfaab3393b7694d4362
Ronald Holshausen <uglyog@gmail.com>
2019-06-06 12:42:06 +1000

    fix: sync command

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -9,7 +9,7 @@ cd $ScriptDir/../website && yarn install && yarn run build
 BUCKET_VAR=${ENVIRONMENT}_BUCKET
 BUCKET=${!BUCKET_VAR}
 
-aws s3 sync ./build/doc-site $BUCKET \
+aws s3 sync $ScriptDir/../website/build/doc-site $BUCKET \
   --acl public-read \
   --cache-control "max-age=60, s-max-age=60, must-revalidate" \
   --delete
