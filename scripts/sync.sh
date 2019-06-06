0d747b57e576ded4966ec9868a853da20c989a4e
Ronald Holshausen <uglyog@gmail.com>
2019-06-06 16:58:34 +1000

    feat: enable prod sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 set -e
-set -x
 
 ScriptDir=$(dirname $0)
 
-ls -l
-
-rm -rf website/build/prod
-mkdir -p website/build/prod
-
-aws s3 sync s3://$dev_BUCKET website/build/prod
-
-find website/build/prod
+aws s3 sync s3://$dev_BUCKET s3://$prod_BUCKET
+  --acl public-read \
+  --cache-control "max-age=60, s-max-age=60, must-revalidate" \
+  --delete
