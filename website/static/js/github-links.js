04bc1a6877e01bda9fcb5f799440cd9e4f45729e
Ronald Holshausen <uglyog@gmail.com>
2019-06-13 13:38:14 +1000

    fix: doc URLs can end in a slash

--- website/static/js/github-links.js
+++ website/static/js/github-links.js
@@ -4,6 +4,9 @@ window.addEventListener('load', function() {
     if (doc.endsWith('.html')) {
       doc = doc.substring(0, doc.length - 5)
     }
+    if (doc.endsWith('/')) {
+      doc = doc.substring(0, doc.length - 1)
+    }
     block.innerHTML = '<a href="https://github.com/pactflow/docs.pactflow.io/blob/master' + doc + '.md">' + 
       block.innerHTML + '&nbsp;<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" /></a>'
   });
