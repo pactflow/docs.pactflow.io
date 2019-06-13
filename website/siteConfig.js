a609fb07931cba8e701ae8e953b4ad041e63ce2a
Ronald Holshausen <uglyog@gmail.com>
2019-06-13 13:34:31 +1000

    chore: added a link to the github file for each page

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -64,6 +64,8 @@ const siteConfig = {
     'https://buttons.github.io/buttons.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-block-buttons.js',
+    'https://code.jquery.com/jquery-3.4.1.min.js',
+    '/js/github-links.js'
   ],
 
   stylesheets: [
---
+++ website/static/js/github-links.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+  document.querySelectorAll('.docLastUpdate').forEach(function(block) {
+    let doc = window.location.pathname;
+    if (doc.endsWith('.html')) {
+      doc = doc.substring(0, doc.length - 5)
+    }
+    block.innerHTML = '<a href="https://github.com/pactflow/docs.pactflow.io/blob/master' + doc + '.md">' + 
+      block.innerHTML + '&nbsp;<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" /></a>'
+  });
+});
