3b39514318be8e11c59ef98ed0885399b582fcac
Ken Ong <kenming.ong@railslice.com>
2019-07-14 09:57:19 +1000

    feat: yarn audit during build. update vulnerable dependencies

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -8,6 +8,12 @@ pushd $ScriptDir/../website
 
 yarn install && yarn run build
 
+if [[ $(yarn audit --summary | grep "High" |  wc -l) -gt 0 ]]; then
+  echo "high vulnerability found"
+  yarn audit --summary
+  exit 1
+fi
+
 BUCKET_VAR=${ENVIRONMENT}_BUCKET
 BUCKET=${!BUCKET_VAR}
 
