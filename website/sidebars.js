e0176169e4c7fdc9f8ee9b03e3ba469bd7c13613
Beth Skurrie <beth@bethesque.com>
2021-02-01 15:01:07 +1100

    chore: update for 1.9.0 onprem release

--- website/sidebars.js
+++ website/sidebars.js
@@ -205,6 +205,7 @@ module.exports = {
           type: "category",
           label: "Releases",
           items: [
+            "docs/on-premises/releases/1.9.0",
             "docs/on-premises/releases/1.8.0",
             "docs/on-premises/releases/1.7.0",
             "docs/on-premises/releases/1.6.1",
