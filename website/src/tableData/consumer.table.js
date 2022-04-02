d6cacf1fa55e3c3a372fa14013516f9ec036a937
Yousaf Nabi <yousafn@gmail.com>
2022-04-03 00:57:54 +0100

    chore: migrate over rest of examples into data table

--- website/src/tableData/consumer.table.js
+++ website/src/tableData/consumer.table.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const tableData = [
   {
-    linkTitle: "React JS website relying on a Products API",
+    linkTitle: "React JS Website tested using Pact to generate consumer pacts",
     linkUrl: "/docs/examples/js/consumer",
     badges: [
       "https://testdemo.pactflow.io/pacts/provider/pactflow-example-provider/consumer/pactflow-example-consumer/latest/badge.svg",
@@ -12,7 +12,8 @@ export const tableData = [
     side: "Consumer",
   },
   {
-    linkTitle: "Example demonstrating how Cypress could be used to generate consumer pacts",
+    linkTitle:
+      "React JS Website tested using Cypress to generate consumer pacts",
     linkUrl: "/docs/examples/cypress",
     badges: [
       "https://testdemo.pactflow.io/pacts/provider/pactflow-example-provider/consumer/example-consumer-cypress/latest/badge.svg",
@@ -22,7 +23,7 @@ export const tableData = [
     side: "Consumer",
   },
   {
-    linkTitle: "ExpressJS API provider",
+    linkTitle: "ExpressJS API provider tested with Pact Verifier",
     linkUrl: "/docs/examples/js/provider",
     badges: [
       "https://testdemo.pactflow.io/pacts/provider/pactflow-example-provider/consumer/pactflow-example-consumer/latest/badge.svg",
@@ -31,6 +32,216 @@ export const tableData = [
     language: "JS/NodeJS",
     useCase: "API",
     side: "Provider",
+  },
+  {
+    linkTitle: ".NET API Provider tested wtih Swashbuckle and Schemathesis against OAS",
+    linkUrl: "/docs/examples/bi-directional/provider/dotnet",
+    badges: [],
+    language: ".NET",
+    useCase: "OpenAPI Spec",
+    side: "Provider",
+  },
+  {
+    linkTitle: "SpringBoot API Provider tested with RestAssured against OAS",
+    linkUrl: "/docs/examples/bi-directional/provider/restassured",
+    badges: [],
+    language: "Java",
+    useCase: "OpenAPI Spec",
+    side: "Provider",
+  },
+  {
+    linkTitle: "ExpressJS API Provider tested with Postman against OAS",
+    linkUrl: "/docs/examples/bi-directional/provider/postman",
+    badges: [],
+    language: "JS",
+    useCase: "OpenAPI Spec",
+    side: "Provider",
+  },
+  {
+    linkTitle: "ExpressJS API Provider tested with Dredd against OAS",
+    linkUrl: "/docs/examples/bi-directional/provider/dredd",
+    badges: [],
+    language: "JS",
+    useCase: "OpenAPI Spec",
+    side: "Provider",
+  },
+  {
+    linkTitle: "Python API Provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/provider/python",
+    badges: [],
+    language: "Python",
+    useCase: "API",
+    side: "Provider",
+  },
+  {
+    linkTitle: "Golang Gin API Provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/golang/python",
+    badges: [],
+    language: "Golang",
+    useCase: "API",
+    side: "Provider",
+  },
+  {
+    linkTitle: ".NET API Provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/dotnet/python",
+    badges: [],
+    language: ".NET",
+    useCase: "API",
+    side: "Provider",
+  },
+  {
+    linkTitle: "Java SOAP API provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/soap/java/provider",
+    badges: [],
+    language: "Java",
+    useCase: "SOAP",
+    side: "Provider",
+  },
+  {
+    linkTitle: "Java Kafka message provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/kafka/java/provider",
+    badges: [],
+    language: "Java",
+    useCase: "Messages",
+    side: "Provider",
+  },
+  {
+    linkTitle: "Java Spring Boot API provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/java/provider-springboot",
+    badges: [],
+    language: "Java",
+    useCase: "API",
+    side: "Provider",
+  },
+  {
+    linkTitle: "ExpressJS AWS Lambda SNS Provider tested with Pact Verifier",
+    linkUrl: "/docs/examples/aws/sns/provider",
+    badges: [],
+    language: "JS/NodeJS",
+    useCase: "AWS/SNS/Messages",
+    side: "Provider",
+  },
+  {
+    linkTitle: "Node API consumer using Mountebank stubs and Pact Bi-Directional BYO adapters to generate Pact contracts",
+    linkUrl: "/docs/examples/bi-directional/consumer/mountebank",
+    badges: [],
+    language: "JS/NodeJS",
+    useCase: "OpenAPI",
+    side: "Consumer",
+  },
+  {
+    linkTitle: "Java API consumer using Wiremock stubs and Pact Bi-Directional BYO adapters to generate Pact contracts",
+    linkUrl: "/docs/examples/bi-directional/consumer/wiremock",
+    badges: [],
+    language: "Java",
+    useCase: "OpenAPI",
+    side: "Consumer",
+  },
+  {
+    linkTitle: "React JS Website using MSW mocks and pact-msw-adapter to generate Pact contracts",
+    linkUrl: "/docs/examples/bi-directional/consumer/msw",
+    badges: [],
+    language: "JS/NodeJS",
+    useCase: "OpenAPI",
+    side: "Consumer",
+  },
+  {
+    linkTitle: "React JS Website using Cypress fixtures and pact-cypress-adapter to generate Pact contracts",
+    linkUrl: "/docs/examples/bi-directional/consumer/cypress",
+    badges: [],
+    language: "JS/NodeJS",
+    useCase: "OpenAPI",
+    side: "Consumer",
+  },
+  {
+    linkTitle: "React JS Website using Nock Record & Replay feature and pact-nock-adapter to generate Pact contracts",
+    linkUrl: "/docs/examples/bi-directional/consumer/recordreplay",
+    badges: [],
+    language: "JS/NodeJS",
+    useCase: "OpenAPI",
+    side: "Consumer",
+  },
+
+  {
+    linkTitle:
+      "Python API Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/python/consumer",
+    badges: [],
+    language: "Python",
+    useCase: "API",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      "Golang API Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/golang/consumer",
+    badges: [],
+    language: "Golang",
+    useCase: "API",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      ".NET (v3.x.x) API Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/golang/dotnet",
+    badges: [],
+    language: ".NET",
+    useCase: "API",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      ".NET (v4.x.x) API Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/bi-directional/consumer/dotnet",
+    badges: [],
+    language: ".NET",
+    useCase: "API",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      "Java Junit API Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/java/consumer/junit",
+    badges: [],
+    language: "Java",
+    useCase: "API",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      "Java SOAP API Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/soap/java/consumer",
+    badges: [],
+    language: "Java",
+    useCase: "SOAP",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      "Java Kafka Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/kafka/java/consumer",
+    badges: [],
+    language: "Java",
+    useCase: "Messages",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      "NodeJS Kafka Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/kafka/js/consumer",
+    badges: [],
+    language: "NodeJS",
+    useCase: "Messages",
+    side: "Consumer",
+  },
+  {
+    linkTitle:
+      "NodeJS AWS Lambda SNS Consumer tested using Pact to generate consumer pacts",
+    linkUrl: "/docs/examples/aws/sns/consumer",
+    badges: [],
+    language: "NodeJS",
+    useCase: "AWS/SNS/Messages",
+    side: "Consumer",
   },
 ];
 
@@ -51,7 +262,7 @@ export const columns = [
     className: "data-table",
   },
   {
-    Header: "Link",
+    Header: "Description",
     accessor: "linkTitle",
     className: "data-table left",
     Cell: ({ cell: { value }, row: { original } }) => (
@@ -67,7 +278,12 @@ export const columns = [
     Cell: ({ row: { original } }) => {
       const badges = original.badges;
       if (!badges) return "";
-      return badges.map((w) =><a href={w.replace('/badge.svg','')}> <img src={w}/></a>);
+      return badges.map((w) => (
+        <a href={w.replace("/badge.svg", "")}>
+          {" "}
+          <img src={w} />
+        </a>
+      ));
     },
   },
 ];
