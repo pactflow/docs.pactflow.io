c917821c5f7b752ec8798c5e635dded890264ee7
Matt Fellows <matt.fellows@onegeek.com.au>
2022-09-05 11:28:17 +1000

    chore: no docker TTY on CI

--- scripts/crawl-site.sh
+++ scripts/crawl-site.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker run -it -e "CONFIG=$(cat crawl/crawlconf.json | jq -r tostring)" \
+docker run -e "CONFIG=$(cat crawl/crawlconf.json | jq -r tostring)" \
 -e API_KEY=${PACTFLOW_ALGOLIA_KEY} \
 -e APPLICATION_ID=${PACTFLOW_ALGOLIA_APP_ID} \
 algolia/docsearch-scraper:v1.13.0
