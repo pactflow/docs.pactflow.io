22f4b614a7081c32ff8610840dd93a26f8971bee
inksprout <candy.g24@gmail.com>
2023-08-07 10:14:52 +1000

    chore: PACT-1241 on prem release version 1.26.0

--- website/sidebars.js
+++ website/sidebars.js
@@ -384,6 +384,7 @@ module.exports = {
           label: 'Releases',
           items: [
 //on-prem-release-placeholder
+            'docs/on-premises/releases/1.26.0',
             'docs/on-premises/releases/1.25.0',
             'docs/on-premises/releases/1.24.0',
             'docs/on-premises/releases/1.23.1',
